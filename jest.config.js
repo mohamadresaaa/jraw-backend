f418c917389760b2a5fb40d2a5a491141a0ac009
mohamadreza mosalli <mohamadresaaa@gmail.com>
2019-12-06 14:37:40 +0330

    fix jest config with eslint

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    roots: ["<rootDir>/tests"],
-    transform: {
-      "^.+\\.tsx?$": "ts-jest",
-    },
-    testRegex: "(/tests/.*|(\\.|/)(test|spec))\\.tsx?$",
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  }
+  roots: ['<rootDir>/tests'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+}
