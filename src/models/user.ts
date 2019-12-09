beddacf97c6af5d52fc333a0b314b3e9029b950f
mohamadreza mosalli <mohamadresaaa@gmail.com>
2019-12-09 18:54:48 +0330

    user schema

---
+++ src/models/user.ts
@@ -0,0 +1,50 @@
+import mongoose, { Schema } from "mongoose"
+
+const userSchema = new Schema({
+  avatar: {
+    default: null,
+    type: String,
+  },
+  bio: {
+    default: null,
+    type: String,
+  },
+  birthday: {
+    default: null,
+    type: Date,
+  },
+  email: {
+    lowercase: true,
+    required: true,
+    trim: true,
+    type: String,
+    unique: true,
+  },
+  fullName: {
+    default: null,
+    type: String,
+  },
+  password: {
+    required: true,
+    type: String,
+  },
+  role: {
+    default: "user",
+    enum: ["user", "admin"],
+    type: String,
+  },
+  status: {
+    default: "deactivated",
+    enum: ["activated", "deactivated", "blocked"],
+    type: String,
+  },
+  username: {
+    lowercase: true,
+    required: true,
+    trim: true,
+    type: String,
+    unique: true,
+  },
+}, { timestamps: true })
+
+export default mongoose.model("User", userSchema)
