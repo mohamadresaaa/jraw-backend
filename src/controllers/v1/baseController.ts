a2c47ae8415b636013336e68825c57775244d2a9
mohamadreza mosalli <mohamadresaaa@gmail.com>
2019-12-09 10:29:18 +0330

    install autoBind and add baseController

---
+++ src/controllers/v1/baseController.ts
@@ -0,0 +1,27 @@
+import autoBind from "auto-bind"
+import { Response } from "express"
+import { ErrorMessage, PublicInfoMessage } from "src/lib/messages"
+
+export default class BaseController {
+    constructor() {
+        autoBind(this)
+    }
+
+    /** Show error message
+     * @param {object} error
+     * @returns error
+     */
+    showErrorMessage(error: ErrorMessage) {
+        throw error
+    }
+
+    /** Show success message
+     * @param {response} res
+     * @param {object} data
+     * @returns {response} res.status(200).json({ message, status, properties })
+     */
+    showSuccessMessage(res: Response, data: PublicInfoMessage) {
+        return res.status(data.status).json(data)
+    }
+
+}
