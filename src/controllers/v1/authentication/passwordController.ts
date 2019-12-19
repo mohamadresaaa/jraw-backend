54645c12517dac667a0a6d879e9c8b393685428d
mohamadreza mosalli <mohamadresaaa@gmail.com>
2019-12-19 13:16:48 +0330

    fix change and reset password

--- src/controllers/v1/authentication/passwordController.ts
+++ src/controllers/v1/authentication/passwordController.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Response } from "express"
 import { ErrorMessage } from "../../../lib/messages"
+import Session from "../../../models/session"
 import User from "../../../models/user"
 import { EAction } from "../../../typings/enum/verificationCode"
 import { IRequest } from "../../../typings/interface/express"
@@ -56,8 +57,13 @@ export default new class PasswordController extends BaseController {
             if (verifyCode) {
                 // Find user with id and update password
                 const user = await User.findById(verifyCode.user)
+
+                // If exists user, handle it
                 if (user) {
                     await user.set({ password }).save()
+
+                    // Remove sessions of user
+                    await Session.deleteMany({ user: user.id })
                 }
 
                 // Expire verification code
@@ -90,7 +96,17 @@ export default new class PasswordController extends BaseController {
 
             // If password is the same
             if (req.user?.comparePassword(oldPassword)) {
+                // Update password
                 await req.user.set({ password: newPassword }).save()
+
+                // Remove sessions of user
+                await Session.deleteMany({ user: req.user.id })
+
+                // Return message
+                return this.infoMessage(res, {
+                    message: "Your password has been successfully changed",
+                    status: 200,
+                })
             }
 
             // Otherwise
