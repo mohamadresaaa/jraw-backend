fe629aad75bbcdcc685c99977ff2b7b9cd507122
mohamadreza mosalli <mohamadresaaa@gmail.com>
2019-12-27 21:07:16 +0330

    update signup service and register controller

--- src/controllers/v1/authentication/registerController.ts
+++ src/controllers/v1/authentication/registerController.ts
@@ -1,29 +1,21 @@
 import { NextFunction, Response } from "express"
-import userRepository from "../../../repositories/userRepository"
 import signUpService from "../../../services/v1/authentication/signUpService"
-import { EAction } from "../../../typings/enum/verificationCode"
 import { IRequest } from "../../../typings/interface/express"
 import BaseController from "../baseController"
 
 export default new class RegisterController extends BaseController {
     /** Sign up user
-     * @param email
-     * @param username
-     * @param password
      * @returns message
      */
     public async handle(req: IRequest, res: Response, next: NextFunction) {
         try {
-
-            // Get email, username, password from req.body and calling User Registration Service
+            /** Get email, username, password from req.body
+             * and calling User Registration Service
+             * @param email
+             * @param username
+             * @param password
+             */
             await signUpService({ ...req.body })
-            // const newUser = await userRepository.create({ ...req.body })
-
-            // Create a verification code for account activation
-            // const verificationCode = await this.setVerificationCode(
-            //         new Date(new Date().setDate(new Date().getDate() + 1)),
-            //         EAction.accountActivation,
-            //         newUser.id)
 
             // Return message
             return this.infoMessage(res, {
--- src/services/v1/authentication/signUpService.ts
+++ src/services/v1/authentication/signUpService.ts
@@ -1,4 +1,7 @@
 import userRepository from "../../../repositories/userRepository"
+import verificationCodeRepository from "../../../repositories/verificationCodeRepository"
+import { EAction } from "../../../typings/enum/verificationCode"
+
 
 export default async (data: { email: string, password: string, username: string }) => {
     try {
@@ -6,7 +9,12 @@ export default async (data: { email: string, password: string, username: string 
         const newUser = await userRepository.create({ ...data })
 
         // Create a verification code for account activation
-
+        const newVerificationCode = await verificationCodeRepository.create({
+            expiryDate: new Date(new Date().setDate(new Date().getDate() + 1)),
+            action: EAction.accountActivation,
+            user: newUser.id
+        })
+        
         // Send verification code to email
     } catch (error) {
         throw error
