37b6e7b562e2b098714441adf99ec1bacc28bd82
mohamadreza mosalli <mohamadresaaa@gmail.com>
2019-12-28 21:16:51 +0330

    sign in service

--- src/controllers/v1/authentication/loginController.ts
+++ src/controllers/v1/authentication/loginController.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Response } from "express"
+import signinService from "src/services/v1/authentication/signinService"
 import User from "../../../models/user"
 import { EStatus } from "../../../typings/enum/user"
 import { IRequest } from "../../../typings/interface/express"
@@ -14,49 +15,16 @@ export default new class LoginController extends BaseController {
      */
     public async local(req: IRequest, res: Response, next: NextFunction) {
         try {
-            // Get [email or username], password
-            const { email, password } = req.body
+            /** Get email, password from req.body
+             * and calling User login Service
+             * @param email
+             * @param password
+             * return publicInfoMessage
+             */
+            const result = await signinService({ ...req.body })
 
-            // Find user
-            const user: IUser | null = await User.findOne({ $or: [{ email }, { username: email }] })
-
-            // If find user, handle it
-            if (user) {
-                // If user is inactive or block
-                if (user.status === EStatus.inactive || user.status === EStatus.block) {
-                    this.errorMessage({
-                        message: user.status === EStatus.inactive ?
-                            "Your account is disabled Please activate your account" :
-                            "Your account has been blocked See support for reviewing your account",
-                        name: "Account status",
-                        status: 403,
-                    })
-                }
-                // If enabled 2 factor auth
-
-                // send verification code
-
-                // If password is the same
-                if (await user.comparePassword(password)) {
-                    // Generate jwt token and save to session, return message and user
-                    return this.infoMessage(res, {
-                        message: "Sign in successfully completed",
-                        properties: { token: await user.generateSession(), ...user.dataTransform() },
-                        status: 200,
-                    })
-                }
-
-                // otherwise, handle it
-                this.errorMessage({
-                    message: "Incorrect email or password",
-                    name: "Unauthorized user",
-                    status: 401,
-                })
-            }
-
-            // If not user is found
-            this.errorMessage(ErrorMessage.errNotFound("User",
-                "Incorrect email or password"))
+            // Return message
+            this.infoMessage(res, result)
         } catch (error) {
             next(error)
         }
---
+++ src/services/v1/authentication/signinService.ts
@@ -0,0 +1,38 @@
+import userRepository from "../../../repositories/userRepository"
+import { EStatus } from "../../../typings/enum/user"
+import { ErrorMessage, PublicInfoMessage } from "./../../../lib/messages"
+
+export default async ({ email, password }: { email: string, password: string }): Promise<PublicInfoMessage> => {
+    try {
+        // Find user
+        const user = await userRepository.single({ $or: [{ email }, { username: email }] })
+
+        // If find user, handle it
+        if (user) {
+            // If user is inactive or block
+            if (user.status === EStatus.inactive || user.status === EStatus.block) {
+                throw new ErrorMessage("Account status",
+                    user.status === EStatus.inactive ?
+                        "Your account is disabled Please activate your account" :
+                        "Your account has been blocked See support for reviewing your account",
+                    403)
+            }
+
+            // If password is the same
+            if (await user.comparePassword(password)) {
+                // Generate jwt token and save to session, return message and user
+                return new PublicInfoMessage("Sign in successfully completed",
+                    200,
+                    { token: await user.generateSession(), ...user.dataTransform() })
+            }
+
+            // Otherwise, handle it
+            throw new ErrorMessage("Unauthorized user", "Incorrect email or password", 401)
+        }
+
+        // Otherwise, user is found return error
+        throw ErrorMessage.errNotFound("User", "Incorrect email or password")
+    } catch (error) {
+        throw error
+    }
+}
