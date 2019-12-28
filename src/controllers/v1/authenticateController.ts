a79e7224e1f8cc4b42f7d9b61a5cfc878c2361a5
mohamadreza mosalli <mohamadresaaa@gmail.com>
2019-12-28 21:26:31 +0330

    add login method to auth controller

--- src/controllers/v1/authenticateController.ts
+++ src/controllers/v1/authenticateController.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Response } from "express"
+import signInService from "../../services/v1/authentication/signInService"
 import signUpService from "../../services/v1/authentication/signUpService"
 import { IRequest } from "../../typings/interface/express"
 import BaseController from "./baseController"
@@ -22,6 +23,26 @@ export default new class AuthenticateController extends BaseController {
                 message: "Your account was successfully registered. Please refer to your email for activation",
                 status: 200,
             })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    /** Sign in user
+     * @returns message and user
+     */
+    public async login(req: IRequest, res: Response, next: NextFunction) {
+        try {
+            /** Get email, password from req.body
+             * and calling User login Service
+             * @param email
+             * @param password
+             * return publicInfoMessage
+             */
+            const result = await signInService({ ...req.body })
+
+            // Return message
+            this.infoMessage(res, result)
         } catch (error) {
             next(error)
         }
