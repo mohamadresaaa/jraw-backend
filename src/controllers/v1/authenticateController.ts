e140690226f410bd6bd9bf664f54f4dc15ca3d1f
mohamadreza mosalli <mohamadresaaa@gmail.com>
2019-12-31 20:40:55 +0330

    completed authentication controller

--- src/controllers/v1/authenticateController.ts
+++ src/controllers/v1/authenticateController.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Response } from "express"
+import passwordRecoveryService from "../../services/v1/authentication/passwordRecoveryService"
+import resetPasswordService from "../../services/v1/authentication/resetPasswordService"
 import signInService from "../../services/v1/authentication/signInService"
 import signUpService from "../../services/v1/authentication/signUpService"
 import { IRequest } from "../../typings/interface/express"
@@ -6,12 +8,12 @@ import BaseController from "./baseController"
 
 export default new class AuthenticateController extends BaseController {
     /** Sign up user
-     * @returns message
+     * @return message
      */
     public async registration(req: IRequest, res: Response, next: NextFunction) {
         try {
             /** Get email, username, password from req.body
-             * and calling user sign up Service
+             * and calling user sign up service
              * @param email
              * @param username
              * @param password
@@ -26,17 +28,56 @@ export default new class AuthenticateController extends BaseController {
     }
 
     /** Sign in user
-     * @returns message and user
+     * @returns message, user
      */
     public async login(req: IRequest, res: Response, next: NextFunction) {
         try {
             /** Get email, password from req.body
-             * and calling User login Service
+             * and calling user login service
              * @param email
              * @param password
-             * return publicInfoMessage
+             * @return publicInfoMessage
              */
             const result = await signInService({ ...req.body })
+
+            // Return message
+            return this.infoMessage(res, result)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    /** Send password recovery email to user email
+     * @return message
+     */
+    public async passwordRecovery(req: IRequest, res: Response, next: NextFunction) {
+        try {
+            /** Get email from req.body
+             * and calling password recovery service
+             * @param email
+             * @return publicInfoMessage
+             */
+            const result = await passwordRecoveryService({ ...req.body })
+
+            // Return message
+            return this.infoMessage(res, result)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    /** Reset password user and remove sessions of user
+     * @return message
+     */
+    public async resetPassword(req: IRequest, res: Response, next: NextFunction) {
+        try {
+            /** Get code, password from req.body
+             * and calling reset password service
+             * @param code
+             * @param password
+             * @return publicInfoMessage
+             */
+            const result = await resetPasswordService({ ...req.body })
 
             // Return message
             return this.infoMessage(res, result)
