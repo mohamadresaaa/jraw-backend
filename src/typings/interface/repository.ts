e85d78c4b690206a5a0dfbfcfbc6711695133a53
mohamadreza mosalli <mohamadresaaa@gmail.com>
2019-12-31 20:45:15 +0330

    remove repositories

--- src/typings/interface/repository.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from "mongoose"
-
-export default interface IRepository {
-    list(limit: number, filter?: any, sort?: string): Promise<Document[]>
-    single(filter: any): Promise<Document|null>
-    create(data: object|Document): Promise<Document>
-    update(filter: object|Document, data: object|Document): Promise<Document|null>
-    delete(filter: object|Document): Promise<void>
-}
