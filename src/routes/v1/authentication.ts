0ca862f1e9be027e95aa03a9250d14af6a73ad8c
mohamadreza mosalli <mohamadresaaa@gmail.com>
2019-12-31 20:43:20 +0330

    update routes

--- src/routes/v1/authentication.ts
+++ src/routes/v1/authentication.ts
@@ -3,28 +3,26 @@ import { checkCode, forgotPassword, login, register, resetPassword } from "../..
 import { validator } from "./../../middleware/validator"
 
 // controllers
+import authenticateController from "../../controllers/v1/authenticateController"
 import accountController from "../../controllers/v1/authentication/accountController"
-import loginController from "../../controllers/v1/authentication/loginController"
-import passwordController from "../../controllers/v1/authentication/passwordController"
-import registerController from "../../controllers/v1/authentication/registerController"
 
 const router = express.Router()
 
 // routes
 router.post("/register",
             validator(register),
-            registerController.handle)
+            authenticateController.registration)
 router.post("/activateAccount",
             validator(checkCode),
             accountController.activation)
 router.post("/login",
             validator(login),
-            loginController.local)
-router.post("/forgotPassword",
+            authenticateController.login)
+router.post("/passwordRecovery",
             validator(forgotPassword),
-            passwordController.forgot)
+            authenticateController.passwordRecovery)
 router.post("/resetPassword",
             validator(resetPassword),
-            passwordController.reset)
+            authenticateController.resetPassword)
 
 export default router
